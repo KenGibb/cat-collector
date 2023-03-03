d32128e22fcbb41830df3675eef8157d4e7c5875
KenGibb <kenjibbo@gmail.com>
2023-03-03 09:16:25 -0600

    Added admin and models and stuff

--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
+from .models import Cat
 
-cats = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+# temporary cats for building templates
+# cats = [
+#     {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#     {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+#     {'name': 'Tubs', 'breed': 'ragdoll', 'description': 'chunky lil guy', 'age': 0},
+# ]
 
 # Create your views here.
-
+# view functions match urls to code (like controllers in Express)
+# define our home view function
 def home(request):
     return render(request, 'home.html')
 
+# about route
 def about(request):
     return render(request, 'about.html')
 
+# index route for cats
 def cats_index(request):
+    # just like we passed data to our templates in express
+    # we pass data to our templates through our view functions
+    # we can gather relations from SQL using our model methods
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', { 'cats': cats })
+
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+
+    return render(request, 'cats/detail.html', { 'cat': cat })
